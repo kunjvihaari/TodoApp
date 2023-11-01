a808c60c76302c492fb8893704be42e159859915
Shivam maurya <95133522+kunjvihaari@users.noreply.github.com>
2023-11-02 01:59:26 +0530

    Delete Desktop/fswd1023/New folder/neww directory

--- Desktop/fswd1023/New folder/neww/client/src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import authReducer from './auth';
-import postReducer from './post';
-
-export const rootReducer = combineReducers({
-    auth:authReducer,
-    post:postReducer
-})
